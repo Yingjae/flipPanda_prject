1fc6aea677bc4f6c53de696b71232fa807ff421f
poiup <dkswodud0531@naver.com>
2022-04-29 15:24:00 +0900

    VO commit

---
+++ FLIPPANDA/src/main/java/com/flippanda/vo/AuctionLogVO.java
@@ -0,0 +1,15 @@
+package com.flippanda.vo;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class AuctionLogVO {
+	private int auctionLogNum;
+	private int auctionNum;
+	private Date auctionDate;
+	private int auctionPrice;	
+}
