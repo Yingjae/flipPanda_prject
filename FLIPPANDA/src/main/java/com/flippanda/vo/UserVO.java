66e20fe7661d0dd611b808bf14f0e9a857b71d52
poiup <dkswodud0531@naver.com>
2022-05-06 12:09:56 +0900

    유저정보 관리(회원가입, 탈퇴, 수정, 받아오기)

--- FLIPPANDA/src/main/java/com/flippanda/vo/UserVO.java
+++ FLIPPANDA/src/main/java/com/flippanda/vo/UserVO.java
@@ -5,26 +5,14 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 public class UserVO {
 	private int userNum;
 	private String userName;
 	private int userPoint;
-	private String userAdress;
+	private String userAddress;
 	private String userId;
 	private String userPw;
 	private String userNick;
-	
-	public void setUserPoint(int userPoint) {
-		this.userPoint = userPoint;
-	}
-	public void setUserAdress(String userAdress) {
-		this.userAdress = userAdress;
-	}
-	public void setUserPw(String userPw) {
-		this.userPw = userPw;
-	}
-	public void setUserNick(String userNick) {
-		this.userNick = userNick;
-	}
 }
