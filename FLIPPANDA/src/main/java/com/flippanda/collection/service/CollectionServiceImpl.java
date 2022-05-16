8d2c5a4151ca07810a67f5181afe58a39ea86705
Yingjae <simbadda96@gmail.com>
2022-05-16 11:12:19 +0900

    collection 커밋

---
+++ FLIPPANDA/src/main/java/com/flippanda/collection/service/CollectionServiceImpl.java
@@ -0,0 +1,48 @@
+package com.flippanda.collection.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flippanda.collection.mapper.CollectionMapper;
+import com.flippanda.vo.MyCollectionVO;
+
+@Service
+public class CollectionServiceImpl implements CollectionService {
+
+	// DAO호출을 위한 선언과 의존성 주입
+	@Autowired
+	private CollectionMapper collectionMapper;
+
+	@Override
+	public List<MyCollectionVO> getAllCollectionList() {
+		return collectionMapper.getAllCollectionList();
+	}
+
+	@Override
+	public List<MyCollectionVO> usersCollectionList(long userNum) {
+		return usersCollectionList(userNum);
+	}
+
+	@Override
+	public void insertMyCollection(MyCollectionVO cVO) {
+		collectionMapper.insertMyCollection(cVO);
+	}
+
+	@Override
+	public void deleteMyCollection(long collectionNum) {
+		collectionMapper.deleteMyCollection(collectionNum);
+	}
+
+	@Override
+	public MyCollectionVO selectMyCollection(long collectionNum) {
+		return collectionMapper.selectMyCollection(collectionNum);
+	}
+
+	@Override
+	public void updateMyCollection(MyCollectionVO cVO) {
+		collectionMapper.updateMyCollection(cVO);
+	}
+	
+}
---
+++ FLIPPANDA/src/main/java/com/flippanda/vo/clikeVO.java
@@ -0,0 +1,12 @@
+package com.flippanda.vo;
+
+import lombok.Data;
+
+@Data
+public class clikeVO {
+
+	private long clikeNum;
+	private long collectionNum;
+	private long userNum;
+}
+						
